002ea3d7f97ca379c04954462b34cda38fc1231e
James Viner Esq <jrviner2000@gmail.com>
2022-10-13 20:49:46 -0400

    Add functionality to quit program, fix formatting

--- guess.py
+++ guess.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 from random import randint as rand
+from os import system, name  # geeksforgeeks.org/clear-screen-python
+import sys
 
 answer = rand(1, 99)
 guess = total = invalid = 0
@@ -10,52 +12,91 @@ def pluralize(number):
     return "" if number == 1 else "es"
 
 
+def clear():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+
+clear()
 print(
     end=''
     f"{answer}\n"
-    "I'm thinking of a number from 1 and 100\n"
-    "Your guess: ")
+    "I'm thinking of a number from 1 and 100. Type 'q' to quit \n"
+    "\nYour guess: ")
 
 while guess != answer:
 
-    try:
-        guess = input()
-        guess = int(guess)
+    guess = input()
 
-    except ValueError:
+    if guess == "q":
+        confirmation = ""
+        clear()
         print(
-            end='' 
-            f"{guess} is not a valid guess. Try again.\n"
-            "Your guess: ")
-        invalid += 1
-        continue
+            end=''
+            "Are you sure you would like to quit?\n"
+            "(Your current game stats will not be saved)\n"
+            "\n(y/n): ")
 
-    finally:
-        total += 1
+        while confirmation != "n":
 
-    if guess < 1 or guess > 100:
-        print(
-            end='' 
-            f"{guess} is outside of the accepted range (1 - 100) "
-            "Try again.\nYour guess: ")
-        invalid += 1
+            confirmation = input()
+
+            if confirmation == "y":
+                clear()
+                sys.exit()
+
+            elif confirmation != "n":
+                clear()
+                print(
+                      end=''
+                      "Invalid response.\n"
+                      "\nAre you sure you would like to quit?\n"
+                      "\n(y/n): ")
+
+        print(end='' "\nYour guess: ")
 
     else:
 
-        if guess > answer:
-            print(
-                end=''
-                f"{guess} is too high. Try again.\n"
-                "Your guess: ")
+        try:
+            guess = int(guess)
 
-        elif guess < answer:
+        except ValueError:
             print(
-                end=''
-                f"{guess} is too low. Try again.\n"
+                end='' 
+                f"{guess} is not a valid guess. Try again.\n"
                 "Your guess: ")
+            invalid += 1
+            continue
+
+        finally:
+            total += 1
+
+        if guess < 1 or guess > 100:
+            print(
+                end='' 
+                f"\n{guess} is outside of the accepted range"
+                " (1 - 100)\n\nTry again.\n"
+                "\nYour guess: ")
+            invalid += 1
 
         else:
-            print(
-                f"{guess} is correct.\n"
-                f"You made {total} total guess{pluralize(total)} and "
-                f"{invalid} invalid guess{pluralize(invalid)}.")
+
+            if guess > answer:
+                print(
+                    end=''
+                    f"\n{guess} is too high. Try again.\n"
+                    "\nYour guess: ")
+
+            elif guess < answer:
+                print(
+                    end=''
+                    f"\n{guess} is too low. Try again.\n"
+                    "\nYour guess: ")
+
+            else:
+                print(
+                    f"{guess} is correct.\n"
+                    f"You made {total} total guess{pluralize(total)} "
+                    f"and {invalid} invalid guess{pluralize(invalid)}.")
